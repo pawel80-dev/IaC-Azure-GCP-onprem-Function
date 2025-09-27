e5f7fe28380bf1197ec668ea14c21f07980a9c86
Pawel <pawel80@gmail.com>
2025-09-27 19:05:51 +0200

    tf_azure_v07

---
+++ terraform/azure/variable.tf
@@ -0,0 +1,11 @@
+variable "azure_rg_name" {
+  description = "Resource Group name"
+  type = string
+  default = ""
+}
+
+variable "azure_region" {
+  description = "Region where all resources will be created"
+  type = string
+  default = ""
+}
